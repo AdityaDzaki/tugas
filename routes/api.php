6d7cf7c6d1b889671f0c8a9201763d4d362f062e
AdityaDzaki <adityadzaki1805@gmail.com>
2024-12-25 18:07:50 +0700

    menambahkan middleware

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,9 @@ use App\Http\Controllers\AuthController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::apiResource("categories", CategoryController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource("categories", CategoryController::class);
+});
+
 Route::post("/login", [AuthController::class, "login"]);
